49991e3ab0c2578ab56ee4d241138875838b7aac
Panlizhi <62243961+Panlizhi@users.noreply.github.com>
2026-02-07 14:41:25 +0800

    Update ImageEncoder

--- FPE_grit/models/caption/FPE_module.py
+++ FPE_grit/models/caption/FPE_module.py
@@ -11,22 +11,111 @@ from torch.nn.functional import normalize
 from math import sqrt 
 from math import ceil
 
-class VisionTransformer(nn.Module):
-    def __init__(self, img_size=224, d_model=512, n_heads=8):
-        super().__init__()
-        self.projection = nn.Conv2d(3, d_model, 1)  
-        self.attn = MultiHeadAttention(d_model, n_heads)
-        h = w = img_size
-        self.pos_embed = nn.Parameter(torch.randn(1, h*w, d_model))
-        
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, 
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, 
+                               kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+
     def forward(self, x):
-        x = self.projection(x)  # [b, d_model, h, w]
-        b, c, h, w = x.shape
-        x = x.view(b, c, h*w).permute(0, 2, 1)  # [b, h*w, c]
-        x = x + self.pos_embed
-        x = self.attn(queries=x, keys=x, values=x)
-        x = x.permute(0, 2, 1).view(b, c, h, w)
+        identity = x
+
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+        return out
+
+
+class ResNet50_ImageEncoder(nn.Module):
+    """
+    ResNet50 
+    """
+
+    def __init__(self, d_model=512):
+        super(ResNet50_ImageEncoder, self).__init__()
+        self.d_model = d_model
+        self.in_channels = 64
+
+        # 384 -> 192 (conv stride=2) -> 96 (maxpool stride=2)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+
+        self.layer1 = self._make_layer(Bottleneck, 64, 3, stride=1)
+        self.layer2 = self._make_layer(Bottleneck, 128, 4, stride=2)
+        self.layer3 = self._make_layer(Bottleneck, 256, 6, stride=2)
+        self.layer4 = self._make_layer(Bottleneck, 512, 3, stride=2)
+
+
+
+        self.channel_proj = nn.Sequential(
+            nn.Conv2d(2048, d_model, kernel_size=1, bias=False),
+            nn.BatchNorm2d(d_model),
+            nn.ReLU(inplace=True)
+        )
+
+        self._init_weights()
+
+    def _make_layer(self, block, out_channels, num_blocks, stride=1):
+
+        downsample = None
+        if stride != 1 or self.in_channels != out_channels * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * block.expansion),
+            )
+
+        layers = []
+
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels * block.expansion
+
+        for _ in range(1, num_blocks):
+            layers.append(block(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x, mask=None):
+        x = self.conv1(x)   
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)     
+
+        x = self.layer1(x)     
+        x = self.layer2(x)     
+        x = self.layer3(x)    
+        x = self.layer4(x)     
+        x = self.channel_proj(x)  
+
         return x
+
 
 
 class ViT_ImageEncoder(nn.Module):
@@ -104,13 +193,10 @@ class ViT_ImageEncoder(nn.Module):
             else:
                 key_padding_mask[:, :mask_seq.size(1)] = mask_seq == 0
                 
-
             if actual_L < self.max_seq_len:
                 key_padding_mask[:, actual_L:] = True
         
-
         x = self.encoder(x, src_key_padding_mask=key_padding_mask)
- 
         x = x[:, :L, :] if L < self.max_seq_len else x
         x = x.permute(0, 2, 1).view(-1, self.d_model, h, w)
         return x
@@ -146,7 +232,7 @@ class Freq_Perturbation_Entropy(Module):
         self.conv_mean = nn.Conv2d(d_model, self.out_channel, kernel_size=1,stride=1, padding=0, bias=False)
 
         
-        if self.visual_type == "CNN":
+        if self.visual_type == "simple_CNN":
 
             self.img_encoder = nn.Sequential(
 
@@ -177,10 +263,12 @@ class Freq_Perturbation_Entropy(Module):
                 dropout=dropout,
                 num_layers=cfg.num_layers
             )
-        elif self.visual_type == "ResNet18":
-            pass
+        elif self.visual_type == "CNN":
+            self.img_encoder = ResNet50_ImageEncoder(d_model=d_model)
+
         else:
             raise ValueError(f"Unsupported visual_type: {self.visual_type}")
+
 
     def forward(self, seq, freq_imag_tensor, freq_imag_mask, mask_pad_seq, mask_seq, mask_x_ca):
 
